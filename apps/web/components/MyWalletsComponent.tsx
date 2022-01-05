dfaa5b18ae41e6743262dbce658f866930403048
randomRadish <marcuspangyuyang@gmail.com>
2022-01-05 20:20:53 +0800

    Implement react-moralis queries for wallet factory

--- apps/web/components/MyWalletsComponent.tsx
+++ apps/web/components/MyWalletsComponent.tsx
@@ -1,10 +1,32 @@
-import Wallet from './Wallet';
+import { useEffect } from "react";
+import { useMoralis, useMoralisQuery } from "react-moralis";
+import { createWalletOptions } from "../utils/web3";
+import Wallet from "./Wallet";
 
 const MyWalletsComponent = () => {
+  const { user } = useMoralis();
+  const {
+    data: walletData,
+    error: walletError,
+    isFetching: walletIsFetching,
+    fetch: walletFetch,
+  } = useMoralisQuery("Wallet", (query) =>
+    query.equalTo("walletCreator", user.get("accounts")[0])
+  ); // get wallets
+
+  useEffect(() => {
+    walletFetch();
+    console.error(walletError);
+  }, [walletFetch]);
+
+  console.log(walletData);
+
   return (
     <div>
       <h2>My Wallets</h2>
-      <Wallet />
+      {walletData.map((wallet, key) => (
+        <Wallet key={key} wallet={wallet} />
+      ))}
     </div>
   );
 };
